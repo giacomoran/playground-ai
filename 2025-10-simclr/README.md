e551fc68b11836578279d9c75ac14b570c37f859
Giacomo Randazzo <giacomoran@gmail.com>
2025-10-31 12:03:23 +0100

    [2025-10-simclr] add README and implement data augmentations

--- 2025-10-simclr/README.md
+++ 2025-10-simclr/README.md
@@ -1,3 +1,26 @@
 # Implementing SimCLR
 
 [paper](https://arxiv.org/abs/2002.05709)
+
+## Notes
+
+- Data augmentation: random cropping and flip (followed by resize back to the original size), random color distorsions, random Gaussian blur.
+- Base encoder is the output after the average pooling layer of ResNet. By default they use ResNet-50.
+- 2-layer MLP projection head to project the representation to a 128-dimensional latent space.
+- NT-Xent loss.
+- Batch size N from 256 to 8192. By default batch size 4096 and train for 100 epochs.
+- LARS optimizer for all batch sizes (since learning might be unstable with SGD). Learning rate of 4.8 (= 0.3 × BatchSize/256) and weight decay of 10−6. Linear warmup for the first 10 epochs, and decay the learning rate with the cosine decay schedule without restarts.
+- They use global batch norm in DDP. In contrastive learning, the affine transformation in the batch norm layer can learn to scale the dimensions along which the two positive samples are correlated, but at test time (or in other batches) you get different batch statistics. This can be ameliorated by using larger effective batches.
+- They train on the original ImageNet (1.2M training images).
+
+## Variant implemented here
+
+The paper reports results on CIFAR-10 across batch size, and temperature (averaged over {0.5, 1.0, 1.5} learning rates). The best top-1 accuracy is achieved with configuration:
+
+- Batch size 2048
+- Temperature 0.5 when training for <300 epochs, 0.1 when training for >300 epochs
+
+On CIFAR-10 they make the following modifications:
+
+- In ResNet-50, they replace the first 7x7 Conv of stride 2 with 3x3 Conv of stride 1, and also remove the first max pooling operation
+- Remove Gaussian Blur from the data augmentations, use 0.5 as color distortion strenght
--- 2025-10-simclr/src/main.py
+++ 2025-10-simclr/src/main.py
@@ -1,6 +1,142 @@
-def main():
-    print("Hello from 2025-10-simclr!")
+import torch
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#: Constants
+
+B = 4
+C = 3
+H = 32
+W = 32
+
+classes = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
+
+#: Utils
+
+
+def get_random_color_distortion(s=1.0):  # s is the strength of color distortion.
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
+    rnd_gray = transforms.RandomGrayscale(p=0.2)
+    color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
+    return color_distort
+
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+#: Model
+
+
+class SimCLR(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x1, x2):
+        B, C, H, W = x1.shape
+
+        # TODO:
+        return x1, x2
+
+
+#: Datasets
+
+
+class SimCLRTrainDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        self.base_dataset = torchvision.datasets.CIFAR10(
+            root="./data", train=True, download=True
+        )
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.RandomResizedCrop(
+                    (H, W), scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333)
+                ),
+                transforms.RandomHorizontalFlip(p=0.5),
+                get_random_color_distortion(0.5),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+    def __getitem__(self, index):
+        image, label = self.base_dataset[index]
+        x1 = self.transform(image)
+        x2 = self.transform(image)
+        return x1, x2, label
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+
+class SimCLRTestDataset(torch.utils.data.Dataset):
+    def __init__(self):
+        self.base_dataset = torchvision.datasets.CIFAR10(
+            root="./data", train=False, download=True
+        )
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        )
+
+    def __getitem__(self, index):
+        image, label = self.base_dataset[index]
+        x = self.transform(image)
+        return x, label
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+
+#: Main
 
 
 if __name__ == "__main__":
-    main()
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+    if torch.backends.mps.is_available():
+        torch.mps.manual_seed(0)
+
+    trainset = SimCLRTrainDataset()
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=B, shuffle=True, num_workers=8
+    )
+
+    testset = SimCLRTestDataset()
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=B, shuffle=False, num_workers=8
+    )
+
+    dataiter = iter(trainloader)
+
+    model = SimCLR()
+
+    for i in range(3):
+        x1, x2, labels = next(dataiter)
+        print(" ".join(f"{classes[labels[j]]:5s}" for j in range(B)))
+
+        grid = torch.cat([x1, x2], dim=0)
+        imshow(torchvision.utils.make_grid(grid, nrow=B))
