a9345c81bbc4ba24797d9a003bc0d1c7b341e712
Giacomo Randazzo <giacomoran@gmail.com>
2025-11-05 09:38:27 +0100

    [2025-10-simclr] switch to CUDA

--- 2025-10-simclr/src/main.py
+++ 2025-10-simclr/src/main.py
@@ -185,12 +185,13 @@ if __name__ == "__main__":
         "truck",
     )
 
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     torch.manual_seed(0)
     np.random.seed(0)
-    if torch.backends.mps.is_available():
-        torch.mps.manual_seed(0)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(0)
+        torch.cuda.manual_seed_all(0)
 
     trainset = SimCLRTrainDataset()
     trainloader = torch.utils.data.DataLoader(
